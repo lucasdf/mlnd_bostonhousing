f683144f0e644a9d89563d0cc6b361471a69f062
Lucas <lucasdiasf@gmail.com>
2016-08-28 11:30:55 -0300

    Work on the first part of notebook

---
+++ boston_housing.py
@@ -0,0 +1,39 @@
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+import visuals as vs # Supplementary code
+from sklearn.cross_validation import ShuffleSplit
+
+# Pretty display for notebooks
+#% matplotlib inline
+
+# Load the Boston housing dataset
+data = pd.read_csv('housing.csv')
+prices = data['MEDV']
+features = data.drop('MEDV', axis = 1)
+    
+# Success
+print "Boston housing dataset has {} data points with {} variables each.".format(*data.shape)
+
+# TODO: Minimum price of the data
+minimum_price = np.amin(prices)
+
+# TODO: Maximum price of the data
+maximum_price = np.amax(prices)
+
+# TODO: Mean price of the data
+mean_price = np.mean(prices)
+
+# TODO: Median price of the data
+median_price = np.median(prices)
+
+# TODO: Standard deviation of prices of the data
+std_price = np.std(prices)
+
+# Show the calculated statistics
+print "Statistics for Boston housing dataset:\n"
+print "Minimum price: ${:,.2f}".format(minimum_price)
+print "Maximum price: ${:,.2f}".format(maximum_price)
+print "Mean price: ${:,.2f}".format(mean_price)
+print "Median price ${:,.2f}".format(median_price)
+print "Standard deviation of prices: ${:,.2f}".format(std_price)
